0a4ea13962ff052ec0280d974dac624a79453600
kdan80 <kierandansey80@gmail.com>
2022-07-17 18:26:12 +0100

    Updated roman_to_integer

--- easy/Roman_to_Integer.js
+++ easy/Roman_to_Integer.js
@@ -1,3 +1,24 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+//
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+//     I can be placed before V (5) and X (10) to make 4 and 9.
+//     X can be placed before L (50) and C (100) to make 40 and 90.
+//     C can be placed before D (500) and M (1000) to make 400 and 900.
+//
+// Given a roman numeral, convert it to an integer.
+
+
+
+// Solution
+// Check for subtractive numerals eg IV, IX first
+// Add them to an integer and remove from the Roman numeral string
+// All remaining numerals must be single values
+// Iterate over them and add to the integer
+// Return the integer
+
 const dict = {
     "i": 1,
     "v": 5,
@@ -39,7 +60,6 @@ var romanToInt = function(s) {
             }
         }
     }
-
 
     console.log(int);
     return int;
