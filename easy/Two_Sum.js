74025cbe159fcaef6adcabfa62cf41dd79039d5a
kdan80 <kierandansey80@gmail.com>
2022-07-17 18:16:14 +0100

    Updated two_sum

--- easy/Two_Sum.js
+++ easy/Two_Sum.js
@@ -1,11 +1,21 @@
+//Given an array of integers, nums, and an integer, target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+
+// Solution
+//
+// For each element in the nums array we need to add it to the remaining elements, in turn, and check if those values are equal to the target
+// We do not need to check against earlier elements in the array
+
 var twoSum = function(nums, target) {
     let arr = [];
     let n, o;
 
     for(let i = 0; i < nums.length; i++){
-        n = i + 1;
-
-        for(let j = n; j <= nums.length; j++){
+        for(let j = i + 1; j <= nums.length; j++){
             if(nums[i] + nums[j] === target) return arr = [i,j];
         }
     }
@@ -15,6 +25,6 @@ var twoSum = function(nums, target) {
 const nums = [1,4,12,16,5];
 const target = 21;
 
-const x = twoSum(nums,target)
+const arr = twoSum(nums,target)
 
-console.log(x);
+console.log(arr);
