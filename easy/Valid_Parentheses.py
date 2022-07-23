87df8fe336e629dc58d85a7dc7e0e0595e080fcd
kdan80 <kierandansey80@gmail.com>
2022-07-23 20:43:43 +0100

    Added Valid...py

---
+++ easy/Valid_Parentheses.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+# This file will need to have the execute permission set
+# chmod +x ./LongestCommonPrefix.py
+
+
+#-----------------------------------------#
+#                                         #
+#  Convert a Roman numeral to an integer  #
+#                                         #
+#-----------------------------------------#
+
+class Solution:
+    def isValid(self, s):
+        stack = []
+        closeToOpen = {
+            ")": "(",
+            "]": "[",
+            "}": "{"}
+
+        for c in s:
+            # If c is in c2o then it is a closing parenthesis
+            if c in closeToOpen:
+                # So we check to see if matches the item on the top of the stack
+                if stack and stack[-1] == closeToOpen[c]:
+                    # If they match remove from the top stack item
+                    stack.pop()
+                else:
+                    # If they don't match we have an invalid string
+                    return False
+            # If c is not in c2o then it is an opening parenthesis and should be added to the stack
+            else:
+                stack.append(c)
+
+        # If we were able to pop all items from the stack we have a valid string
+        return True if not stack else False
+
+s = Solution()
+str = "()]{}"
+res = s.isValid(str)
+print(f'{str} is valid = {res}')
+
+
