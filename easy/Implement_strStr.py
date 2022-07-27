3dc1b113f7d678b9856c0ce27887456648544b01
kdan80 <kierandansey80@gmail.com>
2022-07-27 16:15:23 +0100

    Added missing : to if clause

--- easy/Implement_strStr.py
+++ easy/Implement_strStr.py
@@ -33,11 +33,11 @@ class Solution:
                 for j in range(len(needle)):
                     if haystack[i+j] != needle[j]:
                         break
-                    if j == len(needle) - 1
+                    if j == len(needle) - 1:
                         return i
 
         return -1
 
 s = Solution()
-res = s.strStr('hello', 'll')
+res = s.strStr('ello', 'hello')
 print(res)
