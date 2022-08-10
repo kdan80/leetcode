5cf0b2abe41b7c7139bab4638b241194b9a115f5
kdan80 <kierandansey80@gmail.com>
2022-08-10 15:25:09 +0100

    Added Search_Insert_Position.py

---
+++ easy/Search_Insert_Position.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+# This file will need to have the execute permission set
+# chmod +x ./LongestCommonPrefix.py
+
+
+#------------------------------------------------------------------#
+#                                                                  #
+#  Given a sorted array of distinct integers and a target value,   #
+#  return the index if the target is found. If not, return the     #
+#  index where it would be if it were inserted in order.          #
+#                                                                  #
+#------------------------------------------------------------------#
+
+
+class Solution:
+    def searchInsert(self, nums, target):
+
+        return 0
