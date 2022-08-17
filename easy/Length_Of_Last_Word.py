a848cb5388db5a4dd621feeea28546e914182491
kdan80 <kierandansey80@gmail.com>
2022-08-17 16:09:12 +0100

    Added Length_Of_Last_Word

---
+++ easy/Length_Of_Last_Word.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+# This file will need to have the execute permission set
+# chmod +x ./Length_Of_Last_word.py
+
+
+#--------------------------------------------------#
+#                                                  #
+#  Return the length of the last word in a string  #
+#                                                  #
+#--------------------------------------------------#
+
+
+class Solution:
+    def lengthOfLastWord(self, s):
+
+        i, length = len(s) - 1, 0
+
+        while s[i] == " ":
+            i -= 1
+
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+
+        return length
+
+
+s = Solution()
+
+print(s.lengthOfLastWord("Hello World  "))
