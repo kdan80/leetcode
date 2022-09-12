066ade88084d6928fac12a529684c96ec1d8f15b
kdan80 <kierandansey80@gmail.com>
2022-09-12 11:28:43 +0100

    Added Merge_Two_Sorted_Lists

---
+++ easy/Merge_Two_Sorted_Lists.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+# This file will need to have the execute permission set
+# chmod +x ./Merge_Two_Sorted_Lists.py
+
+
+#-----------------------------------------------------------------------#
+#                                                                       #
+#  You are given the heads of two sorted linked lists list1 and list2.  #
+#  Merge the two lists in a one sorted list. The list should be made    #
+#  by splicing together the nodes of the first two lists.               #
+#  Return the head of the merged linked list.                           #
+#                                                                       #
+#-----------------------------------------------------------------------#
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(list1, list2):
+
+    dummy = ListNode()
+    tail = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            list1 = list.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+
+    if list1:
+        tail.next = list1
+    if list2:
+        tail.next = list2
+
+    return dummy.next
+
